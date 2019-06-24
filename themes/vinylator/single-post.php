c4ae122efee79389cec081880fc2f6b3d7ad50a7
parmentlovski <parmentelot.bryan@hotmail.fr>
2019-06-24 13:06:01 +0200

    ajouts des photos aux articles

--- themes/vinylator/single-post.php
+++ themes/vinylator/single-post.php
@@ -11,7 +11,7 @@
                 <h2><?php echo get_the_title(); ?></h2>
 
                 <div class="row d-flex justify-content-center">
-                    <img class="d-block col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 post-image" src="http://localhost:8080/wp-content/uploads/2019/06/fond_1.jpg" alt="Premier slide" width="960" height="640">
+                    <img class="d-block col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 post-image" src="http://localhost:8080/wp-content/uploads/2019/06/pochette.jpg" alt="Premier slide" width="960" height="640">
                     <div class="post-bloc d-flex flex-column justify-content-center col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
                         <h4 class="text-center"><?php the_excerpt(); ?></h4>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, dolorem excepturi dolore numquam neque enim unde et blanditiis atque rerum nostrum, ab voluptate voluptatem itaque illum assumenda aliquam tempora recusandae.
